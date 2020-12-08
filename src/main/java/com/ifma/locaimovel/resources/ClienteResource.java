0667b9508bb29a3de1d4fc91252f28fbf9273d19
Ferreira <jferreira@acad.ifma.edu.br>
2020-12-08 18:00:01 -0300

    Camada de serviço registro de componente

--- src/main/java/com/ifma/locaimovel/resources/ClienteResource.java
+++ src/main/java/com/ifma/locaimovel/resources/ClienteResource.java
@@ -1,22 +1,35 @@
 package com.ifma.locaimovel.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ifma.locaimovel.entiteis.Cliente;
+import com.ifma.locaimovel.services.ClienteService;
 
 @RestController
 @RequestMapping(value = "/Cliente")
 public class ClienteResource {
-    @GetMapping
-	public ResponseEntity<Cliente> findAll() {
+	@Autowired
+	private ClienteService service;
 
-		Cliente c = new Cliente(1, "maria", "37654328712", "2019-09-3");
+	@GetMapping
+	public ResponseEntity<List<Cliente>> findAll() {
+		List<Cliente> list = service.findAll();
 
-		return ResponseEntity.ok().body(c);
+		return ResponseEntity.ok().body(list);
 
 	}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Cliente> findById(@PathVariable Integer id) {
+		Cliente obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+
+	}
 }
---
+++ src/main/java/com/ifma/locaimovel/services/ClienteService.java
@@ -0,0 +1,30 @@
+package com.ifma.locaimovel.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ifma.locaimovel.entiteis.Cliente;
+import com.ifma.locaimovel.repositoreies.ClienteRepository;
+
+@Service
+public class ClienteService {
+
+	// dependencia para o ClienteRepository
+
+	@Autowired
+	private ClienteRepository repository;
+
+	public List<Cliente> findAll() {
+		return repository.findAll();
+
+	}
+	
+	public Cliente findById(Integer id_nome) {
+		
+		Optional<Cliente> obj = repository.findById(id_nome);
+		return obj.get();
+	}
+}
