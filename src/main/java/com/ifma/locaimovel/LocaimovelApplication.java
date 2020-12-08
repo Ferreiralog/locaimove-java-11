0b7be160003ffc75dbfb225cb992524302d69fbd
Ferreira <jferreira@acad.ifma.edu.br>
2020-12-08 12:24:02 -0300

    criação do projeto locaimovel

---
+++ src/main/java/com/ifma/locaimovel/LocaimovelApplication.java
@@ -0,0 +1,13 @@
+package com.ifma.locaimovel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaimovelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaimovelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ifma/locaimovel/LocaimovelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifma.locaimovel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocaimovelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
